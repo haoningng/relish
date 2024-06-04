1c03814365e4f9078eb7a551e4eb01882ed81b81
haoningng <nick.nghn@gmail.com>
2024-06-04 20:31:46 +1000

    feat: added See More button in Listing Component which appears when scroll to the bottom

--- src/components/Listing.jsx
+++ src/components/Listing.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState }  from 'react';
+import { useEffect, useState, useRef }  from 'react';
 import { useOutletContext } from "react-router-dom";
 import { PropTypes } from 'prop-types'
 import CardsView from './CardsView';
@@ -19,7 +19,39 @@ export default function Listing({ mapOn }) {
     mapOn: PropTypes.bool.isRequired
   };
 
+  const [showSeeMore, setShowSeeMore] = useState(false);
   const [listing, setListing] = useState([])
+
+  function handleSeeMoreClick() {
+    console.log('see more')
+  }
+
+  const listingContainerRef = useRef(null); // Add ref to the scrollable div
+
+  useEffect(() => {
+    const listingContainer = listingContainerRef.current;
+
+    const handleScroll = () => {
+      if (
+        listingContainer.scrollTop + listingContainer.clientHeight >=
+        listingContainer.scrollHeight - 0.1 * listingContainer.scrollHeight
+      ) {
+        setShowSeeMore(true);
+      } else {
+        setShowSeeMore(false);
+      }
+    };
+
+    if (listingContainer) {
+      listingContainer.addEventListener('scroll', handleScroll);
+    }
+
+    return () => {
+      if (listingContainer) {
+        listingContainer.removeEventListener('scroll', handleScroll);
+      }
+    };
+  }, []);
 
   useEffect(() => {
     const options = {
@@ -28,7 +60,7 @@ export default function Listing({ mapOn }) {
           'Content-Type': 'application/json',
       },
       body: JSON.stringify({
-          "endpoint": (`https://api.yelp.com/v3/businesses/search?term=${selectedCuisine}${priceLevel ? `&price=` + `${String(priceLevel)}` :''}&categories=${selectedCuisine}&sort_by=${sort}&radius=${radius}${location ? `&location=` + location + ' VIC Australia' : `&latitude=${coordinate.lat}&longitude=${coordinate.lng}` }&locale=en_AU`),
+          "endpoint": (`https://api.yelp.com/v3/businesses/search?term=${selectedCuisine}${priceLevel ? `&price=` + `${String(priceLevel)}` :''}&categories=${selectedCuisine}&sort_by=${sort}&radius=${radius}${location ? `&location=` + location + ' VIC Australia' : `&latitude=${coordinate.lat}&longitude=${coordinate.lng}` }&locale=en_AU&offset=0`),
           "id": `django-insecure-0ezwicnx+&u=g+d3e2&9-u!c9un559($jq7--dpd*8p-bshh4$`
       })
     }
@@ -45,11 +77,9 @@ export default function Listing({ mapOn }) {
 
   return ( mapOn 
     ? <MapView listing={ listing }/> 
-    : <div className="listing-container">
+    : <div className="listing-container" ref={listingContainerRef}>
         <CardsView listing={ listing }/>
-        <br/>
-        <br/>
-        <br/>
+        <button className={`listing-see-more ${showSeeMore ? 'show' : ''}`} onClick={handleSeeMoreClick}>See More</button>
         <br/>
         <br/>
       </div>
