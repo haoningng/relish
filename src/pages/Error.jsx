27ab4df855eed2998ff3581abc3d43db3ba87de2
yichehnc <yichenc2017@gmail.com>
2024-06-07 10:53:34 +1000

    feat: add an initial Error page

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,1 +1,17 @@
-//TODO
+import { useOutletContext, useNavigate } from "react-router-dom";
+import "../styles/index.css";
+
+function NotFound() {
+  return (
+      <div>
+        <h1>
+              404
+          </h1>
+          <h1 style={{ color: "green" }}>
+              Whoops... Chef Not Found!
+          </h1>
+      </div>
+  );
+}
+
+export default NotFound;
