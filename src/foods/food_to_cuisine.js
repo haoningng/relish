c1be8867ab3162bea9ed369044a342a239c4a529
haoningng <nick.nghn@gmail.com>
2024-06-15 14:12:29 +1000

    feat: detect cuisine type in the restaurant name

--- src/foods/food_to_cuisine.js
+++ src/foods/food_to_cuisine.js
@@ -1,11 +1,10 @@
 export const foodToCuisine = {
   "Breakfast": ["breakfast", "brunch", "breakfast and brunch"],
   "Seafood": ["seafood", "fish"],
-  "Halal": ["halal"],
   "European": ["european"],
+  "Desserts": ["desserts", "ice cream", "icecream", "gelato", "sweet", "yoghurt", "chocolate", "cakeshop" ],
+  "Cafe": ["cafe", "cafes", "cake", "desserts", "coffee", "panini", "focacia", "pie"],
   "Bakery": ["bakeries", "bakery", "bread", "baking"],
-  "Vegan": ["vegan", "vegetarian", "plant-based", "vege"],
-  "Desserts": ["desserts", "ice cream", "icecream", "gelato", "sweet", "yoghurt", "cakeshop" ],
   "Malaysian": ["malaysian", "nasi lemak", "laksa", "char kuey teow", "mamak", "mee goreng", "kopitiam", "satay"],
   "Indonesian": ["indonesia", "indonesian", "soto", "ayam penyet"],
   "Italian": ["italian", "pizza", "pasta", "risotto", "gelato", "tiramisu", "lasagna", "carbonara", "bruschetta", "minestrone", "gnocchi", "panna cotta", "cannoli", "osso buco", "caprese salad", "spaghetti aglio e olio", "calzone", "affogato", "cioppino", "limoncello"],
@@ -19,40 +18,53 @@ export const foodToCuisine = {
   "Thai": ["thai", "pad thai", "green curry", "tom yum soup", "som tam", "mango sticky rice", "red curry", "massaman curry", "satay", "larb", "kai yang", "khao soi", "panang curry", "thai tea", "pad see ew", "thai basil chicken", "papaya salad", "sticky rice", "miang kham", "pla rad prik", "thai coconut soup"],
   "Spanish": ["spanish", "paella", "tapas", "gazpacho", "churros", "tortilla española", "patatas bravas", "jamón ibérico", "croquetas", "empanadas", "flan", "sangria", "pisto", "escabeche", "fabada asturiana", "alioli", "coca", "pulpo a la gallega", "salmorejo", "turrón", "bacalao al pil pil"],
   "Korean": ["korean", "kimchi", "bibimbap", "bulgogi", "tteokbokki", "samgyeopsal", "japchae", "pajeon", "galbi", "soondubu jjigae", "kimchi jjigae", "mandu", "kimchi fried rice", "bossam", "jajangmyeon", "banchan", "yukgaejang", "tteokguk", "sundubu", "dakgalbi", "jeyuk bokkeum"],
-  "Vietnamese": ["vietnamese", "pho", "banh mi", "spring rolls", "bun cha", "banh xeo", "ca phe sua da", "com tam", "bun rieu", "goi cuon", "cha gio", "bun bo hue", "mi quang", "banh canh", "pho ga", "hu tieu", "goi du du", "ca nuong", "thit kho", "banh cuon", "bo la lot"],
-  "MiddleEast": ["middle east", "middle eastern","hummus", "falafel", "shawarma", "tabbouleh", "baba ghanoush", "kebabs", "fattoush", "baklava", "labneh", "manakish", "kibbeh", "musakhan", "kanafeh", "kushari", "warbat", "shakshuka", "mahshi", "ful medames", "zaatar", "muhammara"],
-  "Cafe": ["cafe", "cafes", "cake", "bakery", "desserts", "sandwich", "coffee", "panini", "focacia", "pie", "sandwiches"],
+  "Vietnamese": ["vietnamese", "pho", "banh mi", "saigon", "hatyai", "spring rolls", "bun cha", "banh xeo", "ca phe sua da", "com tam", "bun rieu", "goi cuon", "cha gio", "bun bo hue", "mi quang", "banh canh", "pho ga", "hu tieu", "goi du du", "ca nuong", "thit kho", "banh cuon", "bo la lot"],
+  "MiddleEast": ["middle east", "lebanese", "middle eastern","hummus", "falafel", "shawarma", "tabbouleh", "baba ghanoush", "kebabs", "fattoush", "baklava", "labneh", "manakish", "kibbeh", "musakhan", "kanafeh", "kushari", "warbat", "shakshuka", "mahshi", "ful medames", "zaatar", "muhammara"],
   "Burger": ["burger", "burgers", "grill'd"],
-  "FastFood": ["fastfood", "fast food", "mcdonald", "kfc", "hungry jacks", "hotdogs", "hotdog"],
+  "Mediterranean": ["mediterranean"],
   "Brazilian": ["brazilian", "feijoada", "pão de queijo", "brigadeiro", "moqueca", "acarajé", "coxinha", "farofa", "empadão", "tapioca", "caipirinha", "pastel", "churrasco", "picanha", "tutu de feijão", "coxinha", "quindim", "beijinho", "cuscuz", "vatapá", "pudim"],
   "Turkish": ["turkish", "kebabs", "köfte", "lahmacun", "pide", "börek", "mantı", "çiğ köfte", "tavuk göğsü", "baklava", "lokum", "sarma", "kısır", "pastırma", "simit", "menemen", "tahini", "çöp şiş", "midye dolma", "şakşuka", "çiğ köfte"],
   "Ethiopian": ["ethiopian", "injera", "doro wat", "kitfo", "tibs", "shiro", "ful", "gomen", "tej", "firfir", "berbere", "ayib", "kik alicha", "doro tibs", "fitfit", "siga wat", "misir wat", "atikilt wat", "gored gored", "genfo", "kocho"],
   "Caribbean": ["caribbean", "jerk chicken", "ackee and saltfish", "plantains", "curry goat", "rice and peas", "roti", "callaloo", "oxtail stew", "conch fritters", "sorrel", "griot", "cassava cake", "doubles", "rundown", "pelau", "coconut shrimp", "bajan flying fish", "jamaican patty", "black cake", "fish escovitch"],
   "Moroccan": ["moroccan", "couscous", "tagine", "bastilla", "pastilla", "harira", "briouat", "kefta", "zaalouk", "ras el hanout", "chermoula", "msemen", "rfissa", "maakouda", "taktouka", "mint tea", "mlawi", "b'stilla", "makrout", "seffa", "mrouzia"],
+  "Salad": ["salad", "salads"],
   "Bar": ["bar", "bars", "cocktail", "cocktailbars", "wine", "wine bars"],
-  "International": ["international"],
+  "FastFood": ["fastfood", "fast food", "mcdonald", "kfc", "hungry jacks", "hotdogs", "hotdog"],
+  "Australian": ["australian"],
+  "International": ["international", "brazil"],
+  "Breweries": ["breweries", "cider"],
+  "Takeaways": ["takeaways", "take", "away"],
+  "Groceries": ["woolworths", "iga", "coles", "wholefood", "foodworks", "aldi", "groceries", "metro", "grocery", "market", "grocer"],
+  "Halal": ["halal"],
+  "Vegan": ["vegan", "vegetarian", "plant-based", "vege"],
   "Others": ["restaurants"]
 }
 
-// Function to get cuisine type for a restaurant
 export function getCuisineType(restaurant) {
-  if (typeof restaurant === 'object' && Object.prototype.hasOwnProperty.call(restaurant, 'categories')) {
-    const categories = restaurant.categories.flatMap(c => [
-      c.alias.toLowerCase(), 
-      c.title.toLowerCase().replace("&", "and") // Convert to lowercase & replace "&" with "and"
-    ]);
-    console.log(`categories: ${categories}`)    
+  if (typeof restaurant === 'object') {
+    // Extract relevant information from the restaurant object
+    const categories = restaurant.categories
+      ? restaurant.categories.flatMap(c => [
+          c.alias.toLowerCase(), 
+          c.title.toLowerCase().replace("&", "and")
+        ])
+      : [];
+
+    const name = restaurant.name ? restaurant.name.toLowerCase() : '';
+    const alias = restaurant.alias ? restaurant.alias.toLowerCase() : '';
+
+    // Combine all text to search for keywords
+    const allText = categories.join(' ') + ' ' + name + ' ' + alias;
+    console.log(allText)
+    // Check if any keywords match
     for (const cuisine in foodToCuisine) {
       const keywords = foodToCuisine[cuisine];
-      if (categories.some(category => 
-        keywords.some(keyword => keyword.includes(category)) // Check for partial matches
-      )) {
-          return cuisine;
-        }
-    }    
-  } else {
-    return "Others"; // If no match is found
-
+      if (keywords.some(keyword => allText.includes(keyword))) { // Check for partial matches in the combined text
+        return cuisine;
+      }
+    }
+    return "Others";  
   }
 
+  return "Others"; // Return "Others" if no match is found or restaurant is not an object
 }
