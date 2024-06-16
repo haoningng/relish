451aaf79e723afccbd5b0d93e1e802647dc9f759
yichehnc <yichenc2017@gmail.com>
2024-06-16 18:24:53 +1000

    feat: add ifAchieved to display achieved and unachieved awards

--- src/components/Award.jsx
+++ src/components/Award.jsx
@@ -1,10 +1,12 @@
 import awardImage from '../assets/award.svg';
+import awardInactiveImage from '../assets/award_inactive.svg';
 import "../styles/index.css";
 
-function Award({ title, name }) {
+function Award({ name, title, ifAchieved }) {
+  const containerClass = ifAchieved === true ? awardImage : awardInactiveImage;
   return (
-    <div className="award-container">
-      <img src={awardImage} alt="Award" className="award-emoji" />
+    <div className= 'award-container'>
+      <img src={containerClass} alt="Award" className="award-emoji" />
       <p className="award-name">{name}</p>
       <p className="award-title">{title}</p>
     </div> 
--- src/pages/Awards.jsx
+++ src/pages/Awards.jsx
@@ -4,18 +4,18 @@ import "../styles/index.css";
 
 function Awards() {
   const awardsData = [
-    { id: 1, name: 'Mexican Fiesta', title: 'Visited 5 Mexican restaurants' },
-    { id: 2, name: 'Tom Yum Soup', title: 'Visited 5 Thai restaurants' },
-    { id: 3, name: 'Sushi Samurai', title: 'Visited 5 Japanese restaurants' },
-    { id: 4, name: 'Korean Bibimbap', title: 'Visited 5 Korean restaurants' },
-    { id: 5, name: 'Flat White', title: 'Visited 5 Cafes', },
-    { id: 6, name: 'Fortune Cookie', title: 'Visited 5 Chinese restaurants' },
-    { id: 7, name: 'Buon Appetito', title: 'Visited 5 Italian restaurants' },
-    { id: 8, name: 'Spice Seeker', title: 'Visited 5 Indian restaurants' },
-    { id: 9, name: 'French Feast', title: 'Visited 5 French restaurants' },
-    { id: 10, name: 'Souvlaki Story', title: 'Visited 5 Greek restaurants' },
-    { id: 11, name: 'Bavarian Brewskis', title: 'Visited 5 German restaurants', },
-    { id: 12, name: 'Turkish Delight', title: 'Visited 5 Turkish restaurants' },
+    { id: 1, name: 'Mexican Fiesta', title: 'Visited 5 Mexican restaurants', ifAchieved: false },
+    { id: 2, name: 'Tom Yum Soup', title: 'Visited 5 Thai restaurants', ifAchieved: false },
+    { id: 3, name: 'Sushi Samurai', title: 'Visited 5 Japanese restaurants', ifAchieved: true },
+    { id: 4, name: 'Korean Bibimbap', title: 'Visited 5 Korean restaurants', ifAchieved: false },
+    { id: 5, name: 'Flat White', title: 'Visited 5 Cafes', ifAchieved: true },
+    { id: 6, name: 'Fortune Cookie', title: 'Visited 5 Chinese restaurants', ifAchieved: true },
+    { id: 7, name: 'Buon Appetito', title: 'Visited 5 Italian restaurants', ifAchieved: false },
+    { id: 8, name: 'Spice Seeker', title: 'Visited 5 Indian restaurants', ifAchieved: true },
+    { id: 9, name: 'French Feast', title: 'Visited 5 French restaurants', ifAchieved: false },
+    { id: 10, name: 'Souvlaki Story', title: 'Visited 5 Greek restaurants', ifAchieved: false },
+    { id: 11, name: 'Bavarian Brewskis', title: 'Visited 5 German restaurants', ifAchieved: true },
+    { id: 12, name: 'Turkish Delight', title: 'Visited 5 Turkish restaurants', ifAchieved: true },
     
   ];
 
@@ -26,7 +26,7 @@ function Awards() {
       </div>
       <div className="awards-container">
         {awardsData.map((award) => (
-          <Award key={award.id} name={award.name} title={award.title} />
+          <Award key={award.id} name={award.name} title={award.title} ifAchieved={award.ifAchieved} />
         ))}
       </div>
     </div>
