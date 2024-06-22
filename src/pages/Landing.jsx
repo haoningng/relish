773c6f25013c63b81e8f635dbab90cd3a27b85ba
haoningng <nick.nghn@gmail.com>
2024-06-23 09:22:31 +1000

    feat: add Landing Page + routes

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,1 +1,58 @@
-//TODO
+import { useNavigate } from 'react-router-dom'
+
+export default function Landing() {
+    const navigate = useNavigate();
+    // const cuisineList = [
+    //   'Japanese',
+    //   'Mexican',
+    //   'Indian',
+    //   'Chinese',
+    //   'Italian',
+    //   'Thai',
+    //   'Vegan',
+    //   'Pizza',
+    //   'FastFood',
+    //   'Burgers',
+    //   'Desserts',
+    //   'Asian',
+    //   'Halal', 
+    //   'Chicken',
+    //   'Breakfast',
+    //   'Sandwich',
+    //   'Bakery',
+    //   'American',
+    //   'Seafood',
+    //   'Salad'
+    //   ] // cuisine options
+    return (
+      <div className="landing-container"> 
+        <img 
+          className='landing-img'
+          src={`/landing-background.jpg`}
+          alt={`Background photo of Landing Page`} 
+        />
+        {/* <div className='landing-cuisine-container'>
+        {cuisineList.map(each => {
+          return (
+            <div key={cuisineList.indexOf(each)}>
+                <img 
+                  className='landing-cuisine-img'
+                  src={`/Cuisines/${each}.svg`}
+                  alt={`${each} icon`} 
+                />
+            </div>
+          )
+        })}
+        </div> */}
+        <h1 className='landing-title'>Relish</h1>
+        <button 
+          className='landing-login-btn'
+          onClick={() => navigate('/auth/login')}
+        >Log In</button>
+        <button
+          className='landing-firsttime-btn'
+          onClick={() => navigate('/auth/signup')}
+        >First Time Here?</button>
+      </div>
+    );
+}
