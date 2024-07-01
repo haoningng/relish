0bdbbd649b19fce247f6ff7ada9e7f0c1050f2a3
haoningng <nick.nghn@gmail.com>
2024-07-01 19:05:16 +1000

    style: change styling of text in progress bar

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -157,9 +157,13 @@ export default function Profile() {
         <div className='progress-bar'>
           <div className='progress-left-text'>
             <p className='progress-sum'>{restaurantList ? restaurantList?.length : 0}</p>
-            <p className='progress-checkin-text'>Check-ins</p>
+            <p className='progress-small-text'>Visited</p>
           </div>
           <StepProgressBar progress={restaurantList?.length} />
+          <div className='progress-right-text'>
+            <p className='progress-sum'>100</p>
+            <p className='progress-small-text'>Goal</p>
+          </div>
         </div>
       </div>
       {toggleMapView ?
