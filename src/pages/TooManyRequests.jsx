fc80e7f48c401ebc9f543693fccfc59f9fd1dbe6
haoningng <nick.nghn@gmail.com>
2024-07-05 20:52:42 +1000

    fix: remove createRoot to prevent error - listen to isThrottled state from Redux, if isThrottled is true, render the page, if not send user back to home page - listen for time state from Redux to determine how many seconds to count down from

--- src/pages/TooManyRequests.jsx
+++ src/pages/TooManyRequests.jsx
@@ -1,11 +1,12 @@
-import { useNavigate } from "react-router-dom";
-import {createRoot} from 'react-dom/client';
+import { useNavigate, Navigate } from "react-router-dom";
 import notFoundImage from '../assets/notfound.svg';
 import Countdown from "react-countdown";
 import "../styles/index.css";
+import { useAppSelector } from "../redux/hooks";
 
-function TooManyRequests() {
+export default function TooManyRequests() {
   const navigate = useNavigate();
+  const { isThrottled, time } = useAppSelector(state => state.auth)
 
   function handleBack(event) {
     event.preventDefault();
@@ -14,35 +15,28 @@ function TooManyRequests() {
 
   const renderer = ({ seconds, completed }) => {
     if (completed) {
-      return <span>Time is up!</span>;
+      return <span>Thanks for waiting!</span>;
     } else {
-      return <span>{seconds}</span>;
+      return <span>Please wait for {seconds} seconds</span>;
     }
   };
 
-  createRoot(
-    <Countdown date={Date.now() + 10000} renderer={renderer} />,
-    document.getElementById("root")
-  );
-
-  return (
+  return ( isThrottled ?
     <div className="toomanyrequests-container">
       <h1>429 Too Many Requests</h1>
       <div className="toomanyrequests-message">
         <img src={notFoundImage} alt="toomanyrequests" className="notfound-emoji" />
-        <h2 className='toomanyrequests-label'>Slow Down...<br/>Too Many Requests!</h2>
+        <h2 className='toomanyrequests-label'>
+          Take a breath...<br/>
+          <Countdown date={Date.now() + (time * 1000)} renderer={renderer} />
+        </h2>
       </div>
       <button className="back-button" onClick={handleBack}>
         Back To Finding Food
       </button>
     </div>
+    :
+    // if not throttled / no 429 code then return to Homepage
+    <Navigate to='/' replace/>
   );
-}
-
-export default TooManyRequests;
-
-const rootElement = document.getElementById('root');
-if (rootElement) {
-  const root = createRoot(rootElement);
-  root.render(<TooManyRequests />);
 }
